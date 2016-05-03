bc0ecace353c9bd9dfe1c5a8e708456ff0e2ebe5
Levi Bostian <levi@curiosityio.com>
2016-05-03 09:45:38 -0500

    Initial Commit

---
+++ iOSBlanky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSBlanky
+//
+//  Created by Levi Bostian on 5/3/16.
+//  Copyright © 2016 Curiosity IO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOSBlankyTests/iOSBlankyTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSBlankyTests.swift
+//  iOSBlankyTests
+//
+//  Created by Levi Bostian on 5/3/16.
+//  Copyright © 2016 Curiosity IO. All rights reserved.
+//
+
+import XCTest
+@testable import iOSBlanky
+
+class iOSBlankyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
